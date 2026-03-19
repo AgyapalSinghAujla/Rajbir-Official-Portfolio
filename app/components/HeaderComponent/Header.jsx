a2fc39b0e6c07c1c460b27db1364b6fe34cb6ca7
AgyapalSinghAujla <agyapalsinghaujla@gmail.com>
2026-03-19 11:31:30 +0530

    Header updated

--- app/components/HeaderComponent/Header.jsx
+++ app/components/HeaderComponent/Header.jsx
@@ -1,12 +1,18 @@
 const Header = () =>{
     return(
-        <div>
-            Head
-        </div>
+        <header>
+            <div>
+                <img width={200} height={200} className="ra-official-logo" src="./../RajbirAujlaOfficialProfile.jpeg"/>
+            </div>
+            <div>
+                <h1>Rajbir Aujla</h1>
+            </div>
+            <div>
+                <p>"Where Worlds Fail, Music Speaks 🎙️</p>
+            </div>
+        </header>
     )
-
 };
-
 export default Header;
 
 
