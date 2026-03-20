c3e24e2bd2945e80f074ab6b9ec6aedb2214bb90
AgyapalSinghAujla <agyapalsinghaujla@gmail.com>
2026-03-20 14:39:36 +0530

    Code Optimized for Latest Release

--- app/components/StreamingLinkComponent/LatestReleases.jsx
+++ app/components/StreamingLinkComponent/LatestReleases.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import Link from "next/link";
 
 const releases = [
   {
     id: 1,
     title: "Fade Moon",
     artist: "Rajbir Aujla",
-    platform: "/YouTube.png",
+    platform: "YouTube",
+    platformIcon: "/YouTube.png",
     image: "/Fademoon.jpeg",
     href: "https://youtu.be/skhuAY560BI",
   },
@@ -14,7 +14,8 @@ const releases = [
     id: 2,
     title: "Fade Moon",
     artist: "Rajbir Aujla",
-    platform: "/Spotify.png",
+    platform: "Spotify",
+    platformIcon: "/Spotify.png",
     image: "/Fademoon.jpeg",
     href: "https://open.spotify.com/track/1HboTUIhYhkrwLrJ9twY88",
   },
@@ -22,7 +23,8 @@ const releases = [
     id: 3,
     title: "Fade Moon",
     artist: "Rajbir Aujla",
-    platform: "/Apple-Music.png",
+    platform: "Apple Music",
+    platformIcon: "/Apple-Music.png",
     image: "/Fademoon.jpeg",
     href: "https://music.apple.com/in/album/fade-moon/1881104654",
   },
@@ -30,7 +32,8 @@ const releases = [
     id: 4,
     title: "Fade Moon",
     artist: "Rajbir Aujla",
-    platform: "/Yt-Music.png",
+    platform: "YouTube Music",
+    platformIcon: "/Yt-Music.png",
     image: "/Fademoon.jpeg",
     href: "https://music.youtube.com/watch?v=4bz7oF5Bg5c&si=H3lE_OoC_1TEx6YR",
   },
@@ -38,7 +41,10 @@ const releases = [
 
 const LatestRelease = () => {
   return (
-    <section className="latest-release" aria-labelledby="latest-release-heading">
+    <section
+      className="latest-release"
+      aria-labelledby="latest-release-heading"
+    >
       <h2 id="latest-release-heading" className="sr-only">
         Latest Release
       </h2>
@@ -46,31 +52,34 @@ const LatestRelease = () => {
       <ul className="release-list">
         {releases.map((item, index) => (
           <li key={item.id}>
-            <Link href={item.href} className="release-card">
-            <div className="image-title">
-              <Image
-                src={item.image}
-                alt={item.title}
-                width={50}
-                height={50}
-                className="song-logo"
-                priority={index === 0} 
-              />
-              <div className="title-artist">
-                <p className="release-title">{item.title}</p>
-                <p className="artist-name"> {item.artist}</p>
+            <a
+              href={item.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="release-card"
+            >
+              <div className="image-title">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  width={50}
+                  height={50}
+                  priority={index === 0}
+                />
+                <div className="title-artist">
+                  <p className="release-title">{item.title}</p>
+                  <p className="artist-name"> {item.artist}</p>
+                </div>
               </div>
-            </div>
-            <Image
-              src={item.platform}
-              alt={item.title}
-              width={0} 
-              height={50}
-              className="song-logo"
-              style={{ width: "auto", height: "30px" }}
-              priority={index === 0}
-            />
-            </Link>
+              <Image
+                src={item.platformIcon}
+                alt={item.platform}
+                width={0}
+                height={50}
+                style={{ width: "auto", height: "30px" }}
+                priority={index === 0}
+              />
+            </a>
           </li>
         ))}
       </ul>
